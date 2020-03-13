76f5a3c1c59435fede1699b1f50411648972eb8e
amberm1647 <35271595+amberm1647@users.noreply.github.com>
2020-03-12 20:02:39 -0700

    this builds and runs

--- app/src/main/java/com/example/android/meallogger/SavedMealsViewModel.java
+++ app/src/main/java/com/example/android/meallogger/SavedMealsViewModel.java
@@ -3,39 +3,24 @@ package com.example.android.meallogger;
 import android.app.Application;
 
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
 
-import com.example.android.meallogger.data.Meal;
 import com.example.android.meallogger.data.MealData;
 import com.example.android.meallogger.data.SavedMealsRepository;
-import com.example.android.meallogger.data.Status;
-
-import java.util.List;
 
 public class SavedMealsViewModel extends AndroidViewModel {
-    private SavedMealsRepository mSavedMealsRepository;
-    private LiveData<List<Meal>> mSavedMeals;
-    private LiveData<Status> mLoadingStatus;
+    private SavedMealsRepository mSavedReposRepository;
 
     public SavedMealsViewModel(Application application) {
         super(application);
-        mSavedMealsRepository =
+        mSavedReposRepository =
                 new SavedMealsRepository(application);
     }
 
     public void insertMeal(MealData meal) {
-        mSavedMealsRepository.insertMeal(meal);
+        mSavedReposRepository.insertMeal(meal);
     }
 
-    public void deleteSavedMeal(MealData meal) {
-        mSavedMealsRepository.deleteMeal(meal);
-    }
-
-    public LiveData<List<Meal>> getSavedMeals() {
-        return mSavedMeals;
-    }
-
-    public LiveData<Status> getLoadingStatus() {
-        return mLoadingStatus;
+    public void deleteGitHubRepo(MealData meal) {
+        mSavedReposRepository.deleteMeal(meal);
     }
 }
--- app/src/main/java/com/example/android/meallogger/data/Meal.java
+++ app/src/main/java/com/example/android/meallogger/data/Meal.java
@@ -5,17 +5,11 @@ import java.util.List;
 
 public class Meal {
     public String title;
-    public String type;
-    public String date;
-    public String time;
     public List<MealItem> items;
     public MealNutrients totalNutrients;
 
     Meal(){
         title = new String();
-        type = new String();
-        date = new String();
-        time = new String();
         items = new ArrayList<MealItem>();
         totalNutrients = new MealNutrients();
     }
